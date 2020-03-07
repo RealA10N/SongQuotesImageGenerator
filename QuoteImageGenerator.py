51cfea875b69cfb5647e6f85071d125ad3b86cd7
Alon (PC) <downtown2u@gmail.com>
2020-03-07 07:41:58 +0200

    Converted QuoteImageGenerator.py to TextImageGenerator.py
    Also, updated the fancy_text function and moved it to QuoteImageGenerator.py

--- QuoteImageGenerator.py
+++ QuoteImageGenerator.py
@@ -1,75 +1,26 @@
-from BaseImageGenerator import BaseImageGenerator
+from TextImageGenerator import TextImageGenerator
 from PIL import Image, ImageDraw, ImageFont
 
 
-class QuoteImageGenerator(BaseImageGenerator):
+class QuoteImageGenerator(TextImageGenerator):
 
-    def __init__(self):
+    def __init__(self, text):
         super().__init__()
 
-        self._draw_image = ImageDraw.Draw(self.get_image())
-        
-        self._font = ImageFont.truetype(
-            "fonts/Cocogoose-Classic-Medium.ttf",
-            size = self._config["text"]["size"])
-        
-        self._max_text_width = self._max_text_width()
-
-    
     def fancy_text(self, text):
-        
-        text = text.lower()
+    
+        text = text.lower()              # makes all lowercase
 
-        text = text.replace("\r",". ")
+        text = text.replace("\r",". ")   # remove new lines
         text = text.replace("\n",". ")
 
-        text = " ".join(text.split())
+        text = " ".join(text.split())    # remove whitespaces
 
-        if text[-1] != ".":
-            text += "."
+        if text[0] != '"':               # add " (quotes) and . (dots)
+            text = '"' + text
+        if text[-1] != '"' and text[-2] != '"':
+            text += '"'
+        if text[-2] != ".":
+            text = text[:-2] + '."'
 
         return text
-
-    def draw_text(self, text):
-
-        text = self.fancy_text(text)
-        words = text.split()
-        line_num = 0
-
-        while words:
-            
-            max_index = len(words)
-
-            while True:
-                cur_line = ' '.join(words[:max_index])
-
-                if self._draw_image.textsize(cur_line, self._font)[0] <= self._max_text_width or max_index == 1:
-                    self._draw_line(cur_line, line_num)
-                    words = words[max_index:]
-                    break
-                else:
-                    max_index -= 1
-            
-            line_num += 1
-
-
-    def _draw_line(self, text, line_num=0):
-        
-        x = self._config["text"]["padding"][0]
-        y = self._config["text"]["padding"][1]
-        y += line_num * (self._config["text"]["space-between-lines"] + self._config["text"]["size"]) 
-
-        self._draw_image.text(
-            (x, y),
-            text,
-            font=self._font,
-            fill=self._config["text"]["color"])
-
-
-    def _max_text_width(self):
-        # Returns the maximum width of the text in each line
-
-        width = self._config["image"]["size"][0]
-        width -= 2 * self._config["text"]["padding"][0]
-        width *= self._config["text"]["max-width-percentage"]
-        return width
---
+++ TextImageGenerator.py
@@ -0,0 +1,75 @@
+from BaseImageGenerator import BaseImageGenerator
+from PIL import Image, ImageDraw, ImageFont
+
+
+class TextImageGenerator(BaseImageGenerator):
+
+    def __init__(self):
+        super().__init__()
+
+        self._draw_image = ImageDraw.Draw(self.get_image())
+        
+        self._font = ImageFont.truetype(
+            "fonts/Cocogoose-Classic-Medium.ttf",
+            size = self._config["text"]["size"])
+        
+        self._max_text_width = self._max_text_width()
+
+    
+    def fancy_text(self, text):
+        
+        text = text.lower()
+
+        text = text.replace("\r",". ")
+        text = text.replace("\n",". ")
+
+        text = " ".join(text.split())
+
+        if text[-1] != ".":
+            text += "."
+
+        return text
+
+    def draw_text(self, text):
+
+        text = self.fancy_text(text)
+        words = text.split()
+        line_num = 0
+
+        while words:
+            
+            max_index = len(words)
+
+            while True:
+                cur_line = ' '.join(words[:max_index])
+
+                if self._draw_image.textsize(cur_line, self._font)[0] <= self._max_text_width or max_index == 1:
+                    self._draw_line(cur_line, line_num)
+                    words = words[max_index:]
+                    break
+                else:
+                    max_index -= 1
+            
+            line_num += 1
+
+
+    def _draw_line(self, text, line_num=0):
+        
+        x = self._config["text"]["padding"][0]
+        y = self._config["text"]["padding"][1]
+        y += line_num * (self._config["text"]["space-between-lines"] + self._config["text"]["size"]) 
+
+        self._draw_image.text(
+            (x, y),
+            text,
+            font=self._font,
+            fill=self._config["text"]["color"])
+
+
+    def _max_text_width(self):
+        # Returns the maximum width of the text in each line
+
+        width = self._config["image"]["size"][0]
+        width -= 2 * self._config["text"]["padding"][0]
+        width *= self._config["text"]["max-width-percentage"]
+        return width
