ea34eecc3757254a0735481d0977ed13c6cf0cb4
Alon (PC) <downtown2u@gmail.com>
2020-03-07 02:57:02 +0200

    Added background color generator and config.json loader

---
+++ image-generator.py
@@ -0,0 +1,27 @@
+import json
+import colorsys
+import random
+
+
+def load_config():
+    # Uses json package
+
+    with open("config.json") as config_file:
+        config = json.load(config_file)
+
+
+def generate_background_color(config=load_config()):
+    # Uses random and colorsys packages
+
+    # Generate a random color
+    color = colorsys.hsv_to_rgb(
+        random.random(),               # hue
+        config["image"]["saturation"], # saturation
+        config["image"]["lightness"])  # lightness, brightness
+
+    # Convert to int from float
+    color = list(color)
+    for i, value in enumerate(color):
+        color[i] = int(value)
+    
+    return tuple(color)
